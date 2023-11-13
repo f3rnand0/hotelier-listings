d0026428c0b3022ed2c7c3e8843b7c4b5b280ebf
Fernando Guerra <fernando.guerra@nordstrom.com>
2023-11-13 16:31:39 -0500

    First commit

---
+++ src/test/kotlin/com/challenge/hotelierlistings/HotelierListingsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.challenge.hotelierlistings
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HotelierListingsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
