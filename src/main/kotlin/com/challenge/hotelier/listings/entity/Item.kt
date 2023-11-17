71329566d5ce6a4bb4e1a8eb6d1468a384bce87d
Fernando Guerra <fernando.guerra@nordstrom.com>
2023-11-17 12:19:17 -0500

    Fixed some database sequence issues. Updated Dockerfile to include updated DATABASE_URL environment variable. Removed unused configuration in pom.xml

--- src/main/kotlin/com/challenge/hotelier/listings/entity/Item.kt
+++ src/main/kotlin/com/challenge/hotelier/listings/entity/Item.kt
@@ -3,10 +3,10 @@ package com.challenge.hotelier.listings.entity
 import jakarta.persistence.*
 
 @Entity
-@SequenceGenerator(name = "seq_PK", sequenceName = "ITEM_SEQUENCE", initialValue = 1000)
+@SequenceGenerator(name = "seq_pk_item", sequenceName = "ITEM_SEQUENCE", initialValue = 1000, allocationSize = 1)
 class Item(
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_PK")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_pk_item")
     var id: Long? = null,
     val name: String,
     val rating: Int,
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-MERGE INTO hotelier (id, first_name, last_name) KEY(id) VALUES (10, 'Elon', 'Musk');
-MERGE INTO hotelier (id, first_name, last_name) KEY(id) VALUES (20, 'Steve', 'Jobs');
-MERGE INTO hotelier (id, first_name, last_name) KEY(id) VALUES (30, 'Bill', 'Gates');
+MERGE INTO hotelier (id, first_name, last_name) KEY(id) VALUES (1, 'Elon', 'Musk');
+MERGE INTO hotelier (id, first_name, last_name) KEY(id) VALUES (2, 'Steve', 'Jobs');
+MERGE INTO hotelier (id, first_name, last_name) KEY(id) VALUES (3, 'Bill', 'Gates');
 
-MERGE INTO location (id, city, state, country, zip_code, address) KEY(id) VALUES (10, 'Quito', 'Pichincha', 'Ecuador', 17090, 'San Francisco y Los Guabos Lote 1');
-MERGE INTO location (id, city, state, country, zip_code, address) KEY(id) VALUES (20, 'Lisbon', '', 'Portugal', 12345, 'Av. António Augusto de Aguiar 31, 1069-413');
-MERGE INTO location (id, city, state, country, zip_code, address) KEY(id) VALUES (30, 'Valladolid', 'Castile and León', 'España', 47013, 'C. del Hornija');
-MERGE INTO location (id, city, state, country, zip_code, address) KEY(id) VALUES (40, 'Düsseldorf', 'North Rhine-Westphalia', 'Germany', 40221, 'Kesselstraße 5-7');
+MERGE INTO location (id, city, state, country, zip_code, address) KEY(id) VALUES (1, 'Quito', 'Pichincha', 'Ecuador', 17090, 'San Francisco y Los Guabos Lote 1');
+MERGE INTO location (id, city, state, country, zip_code, address) KEY(id) VALUES (2, 'Lisbon', '', 'Portugal', 12345, 'Av. António Augusto de Aguiar 31, 1069-413');
+MERGE INTO location (id, city, state, country, zip_code, address) KEY(id) VALUES (3, 'Valladolid', 'Castile and León', 'España', 47013, 'C. del Hornija');
+MERGE INTO location (id, city, state, country, zip_code, address) KEY(id) VALUES (4, 'Düsseldorf', 'North Rhine-Westphalia', 'Germany', 40221, 'Kesselstraße 5-7');
 
-MERGE INTO item (id, name, rating, category, image, reputation, reputation_badge, price, availability, location_id, hotelier_id) KEY(id) VALUES (10, 'Marriot', 5, 0, '/images/marriot.png', 950, 0, 1200, 100, 10, 10);
-MERGE INTO item (id, name, rating, category, image, reputation, reputation_badge, price, availability, location_id, hotelier_id) KEY(id) VALUES (20, 'Hilton Colon', 5, 1, '/images/hilton.png', 900, 0, 1000, 110, 20, 20);
+MERGE INTO item (id, name, rating, category, image, reputation, reputation_badge, price, availability, location_id, hotelier_id) KEY(id) VALUES (1, 'Marriot', 5, 0, '/images/marriot.png', 950, 0, 1200, 100, 1, 1);
+MERGE INTO item (id, name, rating, category, image, reputation, reputation_badge, price, availability, location_id, hotelier_id) KEY(id) VALUES (2, 'Hilton Colon', 5, 1, '/images/hilton.png', 900, 0, 1000, 110, 2, 2);
