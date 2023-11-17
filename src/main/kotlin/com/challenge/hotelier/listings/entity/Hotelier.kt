2c914e84fbc443c72f67dda18b27ac75733a8a91
Fernando Guerra <fgucutito@gmail.com>
2023-11-17 12:19:17 -0500

    Fixed some database sequence issues. Updated Dockerfile to include updated DATABASE_URL environment variable. Removed unused configuration in pom.xml

--- src/main/kotlin/com/challenge/hotelier/listings/entity/Hotelier.kt
+++ src/main/kotlin/com/challenge/hotelier/listings/entity/Hotelier.kt
@@ -3,10 +3,10 @@ package com.challenge.hotelier.listings.entity
 import jakarta.persistence.*
 
 @Entity
-@SequenceGenerator(name = "seq_PK", sequenceName = "HOTELIER_SEQUENCE", initialValue = 1000)
+@SequenceGenerator(name = "seq_pk_hotelier", sequenceName = "HOTELIER_SEQUENCE", initialValue = 1000, allocationSize = 1)
 data class Hotelier(
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_PK")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_pk_hotelier")
     var id: Long? = null,
     @Column(name="first_name")
     val firstName: String,
--- src/main/kotlin/com/challenge/hotelier/listings/entity/Location.kt
+++ src/main/kotlin/com/challenge/hotelier/listings/entity/Location.kt
@@ -3,10 +3,10 @@ package com.challenge.hotelier.listings.entity
 import jakarta.persistence.*
 
 @Entity
-@SequenceGenerator(name = "seq_PK", sequenceName = "LOCATION_SEQUENCE", initialValue = 1000)
+@SequenceGenerator(name = "seq_pk_location", sequenceName = "LOCATION_SEQUENCE", initialValue = 1000, allocationSize = 1)
 data class Location(
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_PK")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_pk_location")
     var id: Long? = null,
     val city: String,
     val state: String,
